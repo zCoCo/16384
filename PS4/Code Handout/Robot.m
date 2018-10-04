9259e3371a234f488376645f039a978e22864e47
zCoCo <connor.caxd@gmail.com>
2018-10-04 18:57:57 -0400

    IK init

--- PS4/Code Handout/Robot.m
+++ PS4/Code Handout/Robot.m
@@ -180,5 +180,67 @@ classdef Robot
             end
 % --------------- END STUDENT SECTION ------------------------------------
         end % #jacobians
+        
+        function thetas = inverse_kinematics(robot, initial_thetas, goal_position)
+            % Returns the joint angles which minimize a simple squared-distance
+            % cost function.
+
+            % Make sure that all the parameters are what we're expecting.
+            % This helps catch typos and other lovely bugs.
+            if size(initial_thetas, 1) ~= robot.dof || size(initial_thetas, 2) ~= 1
+                error('Invalid initial_thetas: Should be a column vector matching robot DOF count, is %dx%d.', size(initial_thetas, 1), size(initial_thetas, 2));
+            end
+
+            if (size(goal_position, 1) ~= 2 && size(goal_position, 1) ~= 3) || ...
+               size(goal_position, 2) ~= 1
+                error('Invalid goal_position: Should be a 2 or 3 length column vector, is %dx%d.', size(goal_position, 1), size(goal_position, 2));
+            end
+
+            % Allocate a variable for the joint angles during the optimization;
+            % begin with the initial condition
+            thetas = initial_thetas;
+
+            % Step size for gradient update
+            step_size = 0.1;
+
+            % Once the norm (magnitude) of the computed gradient is smaller than
+            % this value, we stop the optimization
+            stopping_condition = 0.00005;
+
+            % Also, limit to a maximum number of iterations.
+            max_iter = 100;
+            num_iter = 0;
+
+% --------------- BEGIN STUDENT SECTION ----------------------------------
+            % Run gradient descent optimization
+            while (num_iter < max_iter)
+
+                % Compute the gradient for either an [x;y] goal or an
+                % [x;y;theta] goal, using the current value of 'thetas'.
+                % TODO fill in the gradient of the squared distance cost function
+                % HINT use the answer for theory question 2, the
+                % 'robot.end_effector' function, and the 'robot.jacobians'
+                % function to help solve this problem
+                if (size(goal_position, 1) == 2) % [x;y] goal
+                    cost_gradient = zeros(robot.dof, 1);
+                else % [x;y;theta] goal
+                    cost_gradient = zeros(robot.dof, 1);
+                end
+
+                % Update 'thetas'
+                % TODO
+
+                % Check stopping condition, and return if it is met.
+                % TODO
+
+                num_iter = num_iter + 1;
+            end
+% --------------- END STUDENT SECTION ------------------------------------
+        end % #inverse_kinematics
+        
+        % Shorthand for preforming the inverse kinematics.
+        function ik = ik(robot, initial_thetas, goal_position)
+            ik = robot.inverse_kinematics(robot, initial_thetas, goal_position);
+        end
     end % methods
 end % classdef

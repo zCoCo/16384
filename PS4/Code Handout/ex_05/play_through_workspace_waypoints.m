76eabfe6480c9dea311ff527d1aea066904074fe
zCoCo <connor.caxd@gmail.com>
2018-10-05 04:02:39 -0400

    5

--- PS4/Code Handout/ex_05/play_through_workspace_waypoints.m
+++ PS4/Code Handout/ex_05/play_through_workspace_waypoints.m
@@ -40,15 +40,15 @@ initial_thetas = fbk.position'; % (The transpose turns the feedback into a colum
 %%    seconds.  The trajectories taken should result in straight-line workspace
 %%    trajectories.
 n_points = 100 * segment_duration; % commands/sec * sec
-trajectories = zeros(size(initial_thetas,1), n_points, size(workspace_positions,2)+1);
+trajectories = zeros(size(initial_thetas,1), n_points, size(workspace_positions,2));
 th1 = robot.ik(initial_thetas, workspace_positions(:,1));
-trajectories(:,:,1) = linear_workspace_trajectory(initial_thetas, th1, n_points);
+trajectories(:,:,1) = linear_workspace_trajectory(robot, initial_thetas, workspace_positions(:,1), n_points);
 
 i = 2;
 th_prev = th1;
-while(i <= (size(workspace_positions,2)+1))
+while(i <= size(workspace_positions,2))
     thi = robot.ik(th_prev, workspace_positions(:,i-1));
-    trajectories(:,:,i) = linear_joint_trajectory(thi, workspace_positions(:,i), n_points);
+    trajectories(:,:,i) = linear_workspace_trajectory(robot, thi, workspace_positions(:,i), n_points);
     th_prev = thi;
 i = i+1
 end
@@ -67,7 +67,7 @@ logFile = robot_hardware.startLog('file', fullfile(currentDir, 'workspace_waypoi
 %%        % Wait a little bit to send at ~100Hz.
 %%        pause(0.01);
 %%    end
-for i = 1:(size(workspace_positions,2)+1)
+for i = 1:size(workspace_positions,2)
     for j = 1:n_points
         % Send command to the robot
         cmd.position = trajectories(:,j,i)'; % transpose turns column into row vector for commands

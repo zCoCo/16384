e506f82514a5baa40bcdaa396458a76c47a58845
zCoCo <connor.caxd@gmail.com>
2018-12-09 20:54:57 -0500

    CJTs with Constant Velocity Completed

--- Capstone/CJT_test_sandbox.m
+++ Capstone/CJT_test_sandbox.m
@@ -1,24 +1,24 @@
 function CJT_test_sandbox()
     tic
-    traj = CJT(csvread('sine.csv'), 1000, 10, 10, 250);
+    traj = CJT(csvread('sine.csv'), 1000, 10, 2, 250);
     dt = traj.data.timestep;
     toc
     traj.dist(end)
     traj.params
     
-    As = zeros(size(traj.params.tcrit));
-    for(i = 1:length(traj.params.tcrit)-1)
-        traj.params.tcrit(i);
-        j = find(traj.data.ts >= traj.params.tcrit(i));
-        As(i) = sum(traj.data.vs(1:j(1)))*dt;
-    end
-    As(end) = sum(traj.data.vs)*dt;
-    As
-    
-    vp = traj.params.vpeak
-    ap = traj.params.apeak
-    jm = traj.jmax
-    sc = vp*(ap^2 + jm*vp)/(2*ap*jm)
+%     As = zeros(size(traj.params.tcrit));
+%     for(i = 1:length(traj.params.tcrit)-1)
+%         traj.params.tcrit(i);
+%         j = find(traj.data.ts >= traj.params.tcrit(i));
+%         As(i) = sum(traj.data.vs(1:j(1)))*dt;
+%     end
+%     As(end) = sum(traj.data.vs)*dt;
+%     As
+%     
+%     vp = traj.params.vpeak
+%     ap = traj.params.apeak
+%     jm = traj.jmax
+%     sc = vp*(ap^2 + jm*vp)/(2*ap*jm)
     
     
     % Compute Numerical Derivatives:
